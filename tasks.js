804533b454f2961742ef8d501a28b78e32bdea6e
BAtabaiev <87579276+BAtabaiev@users.noreply.github.com>
2022-06-21 16:26:27 +0300

    Add files via upload

---
+++ tasks.js
@@ -0,0 +1,57 @@
+// write your code here
+//=================1st Task===================
+
+const arr = ['1', '2', '4', '5'];
+function getMaxEvenElement(array) {
+    const getEven = array.reduce((p, c) => {
+        if (c % 2 === 0) {
+            p.push(c);
+        }
+        return  [...p];
+    }, []);
+    return Math.max(...getEven);
+}
+console.log(getMaxEvenElement(arr));
+
+//====================2nd Task================
+
+let a = 3;
+let b = 5;
+
+[a, b] = [b, a];
+
+console.log(a);
+console.log(b);
+
+//====================3rd Task================
+function getValue(value) {
+    if(value === null || value === undefined) {
+        return "-";
+    } else {
+        return value;
+    }
+  
+}
+
+console.log(getValue(0));
+console.log(getValue(4));
+console.log(getValue('someText'));
+console.log(getValue(null));
+console.log(getValue(undefined));
+
+
+
+
+//====================4th Task===================
+
+const arrayOfArrays = [
+    ['name', 'Dan'],
+    ['age', '21'],
+    ['city', 'Lviv'],
+];
+
+function getObjFromArray(arrays) {
+    arrays.forEach(array => {
+        Object.assign
+    });
+} 
